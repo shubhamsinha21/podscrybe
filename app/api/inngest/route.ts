ce6047e7b7293dced9ea989e3db4cdf12f16e871
rozer <shubhamsinha07084@gmail.com>
2025-12-02 17:28:07 +0530

    feat: update API route to serve Inngest functions including helloWorld

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,10 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
+import { helloWorld } from "@/inngest/functions/helloworld";
 
-// Create an API that serves zero functions
+// Create an API that serves Inngest functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     /* your functions will be passed here later! */
+    helloWorld
   ],
 });
