a9d9e2af162acdf3d9f54739e60792225a91344e
rozer <shubhamsinha07084@gmail.com>
2025-12-07 00:52:34 +0530

    feat: resolved import error for podcastUploader function

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,12 +1,16 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
+import { tello } from "@/inngest/functions/tello";
 import { helloWorld } from "@/inngest/functions/helloworld";
+import { podcastProcessor } from "@/inngest/functions/podcast-processor";
 
 // Create an API that serves Inngest functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     /* your functions will be passed here later! */
-    helloWorld
+    tello,
+    helloWorld,
+    podcastProcessor
   ],
 });
